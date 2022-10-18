b3fb3493e5ab5bc349f7b1c17c6ae8e0e46cc499
Joel Ek <contact@joelek.se>
2022-10-18 22:22:20 +0200

    0.5.1

--- dist/lib/state.test.js
+++ dist/lib/state.test.js
@@ -1,31 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const wtf_1 = require("@joelek/wtf");
+const wtf = require("@joelek/wtf");
 const state_1 = require("./state");
-(0, wtf_1.suite)("state", (suite) => {
-    suite.case(`It should not output undefined member values in object values.`, (assert) => {
-        let state = (0, state_1.stateify)({ required: undefined });
-        assert.equals(state.value(), {});
-    });
-    suite.case(`It should support updating optional object members to undefined values.`, (assert) => {
-        let state = (0, state_1.stateify)({ data: { page: "" } });
-        state.update({ data: undefined });
-        assert.equals(state.value(), { data: {} });
-    });
-    suite.case(`It should support updating optional array members to undefined values.`, (assert) => {
-        let state = (0, state_1.stateify)({ data: [] });
-        state.update({ data: undefined });
-        assert.equals(state.value(), { data: [] });
-    });
-    suite.case(`It should initialize optional members lazily when updated.`, (assert) => {
-        let state = (0, state_1.stateify)({});
-        state.update({ optional: undefined });
-        let optional = state.member("optional", false);
-        assert.equals(optional.value(), undefined);
-    });
-    suite.case(`It should initialize optional members lazily when accessed.`, (assert) => {
-        let state = (0, state_1.stateify)({});
-        let optional = state.member("optional", false);
-        assert.equals(optional.value(), false);
-    });
+wtf.test(`It should not output undefined member values in object values.`, (assert) => {
+    let state = (0, state_1.stateify)({ required: undefined });
+    assert.equals(state.value(), {});
+});
+wtf.test(`It should support updating optional object members to undefined values.`, (assert) => {
+    let state = (0, state_1.stateify)({ data: { page: "" } });
+    state.update({ data: undefined });
+    assert.equals(state.value(), { data: {} });
+});
+wtf.test(`It should support updating optional array members to undefined values.`, (assert) => {
+    let state = (0, state_1.stateify)({ data: [] });
+    state.update({ data: undefined });
+    assert.equals(state.value(), { data: [] });
+});
+wtf.test(`It should initialize optional members lazily when updated.`, (assert) => {
+    let state = (0, state_1.stateify)({});
+    state.update({ optional: undefined });
+    let optional = state.member("optional", false);
+    assert.equals(optional.value(), undefined);
+});
+wtf.test(`It should initialize optional members lazily when accessed.`, (assert) => {
+    let state = (0, state_1.stateify)({});
+    let optional = state.member("optional", false);
+    assert.equals(optional.value(), false);
 });
