072128bb78a03dee1d5bfb8e92db7dff96603d60
Joel Ek <contact@joelek.se>
2024-02-29 11:53:29 +0100

    Generate dist artifacts.

--- dist/lib/utils.d.ts
+++ dist/lib/utils.d.ts
@@ -1,1 +1,46 @@
 export declare function getOrderedIndex<A>(array: Array<A>, collator: (value: A) => number): number;
+type IterableWeakMapEntry<A extends object, B> = {
+    keyref: WeakRef<A>;
+    value: B;
+};
+export declare class IterableWeakMap<A extends object, B> implements Map<A, B> {
+    protected keyrefs: Set<WeakRef<A>>;
+    protected registry: FinalizationRegistry<WeakRef<A>>;
+    protected map: WeakMap<A, IterableWeakMapEntry<A, B>>;
+    protected current_size: number;
+    constructor(iterable?: Iterable<[A, B]>);
+    get size(): number;
+    [Symbol.toStringTag]: "IterableWeakMap";
+    [Symbol.iterator](): IterableIterator<[A, B]>;
+    entries(): Generator<[A, B]>;
+    keys(): Generator<A>;
+    values(): Generator<B>;
+    clear(): void;
+    delete(key: A): boolean;
+    forEach(callback: (value: B, key: A, map: Map<A, B>) => void, self?: any): void;
+    get(key: A): B | undefined;
+    has(key: A): boolean;
+    set(key: A, value: B): this;
+}
+type IterableWeakSetEntry<A extends object> = {
+    keyref: WeakRef<A>;
+};
+export declare class IterableWeakSet<A extends object> implements Set<A> {
+    protected keyrefs: Set<WeakRef<A>>;
+    protected registry: FinalizationRegistry<WeakRef<A>>;
+    protected map: WeakMap<A, IterableWeakSetEntry<A>>;
+    protected current_size: number;
+    constructor(iterable?: Iterable<A>);
+    get size(): number;
+    [Symbol.toStringTag]: "IterableWeakSet";
+    [Symbol.iterator](): IterableIterator<A>;
+    entries(): Generator<[A, A]>;
+    keys(): Generator<A>;
+    values(): Generator<A>;
+    add(key: A): this;
+    clear(): void;
+    delete(key: A): boolean;
+    forEach(callback: (value: A, key: A, set: Set<A>) => void, self?: any): void;
+    has(key: A): boolean;
+}
+export {};
--- dist/lib/utils.js
+++ dist/lib/utils.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getOrderedIndex = void 0;
+exports.IterableWeakSet = exports.IterableWeakMap = exports.getOrderedIndex = void 0;
 function getOrderedIndex(array, collator) {
     function recursive(offset, length) {
         if (length <= 0) {
@@ -22,4 +22,207 @@ function getOrderedIndex(array, collator) {
     return recursive(0, array.length);
 }
 exports.getOrderedIndex = getOrderedIndex;
+;
+class IterableWeakMap {
+    keyrefs;
+    registry;
+    map;
+    current_size;
+    constructor(iterable) {
+        this.keyrefs = new Set();
+        this.registry = new FinalizationRegistry((keyref) => {
+            this.keyrefs.delete(keyref);
+            this.current_size -= 1;
+        });
+        this.map = new WeakMap();
+        this.current_size = 0;
+        for (let [key, value] of iterable ?? []) {
+            this.set(key, value);
+        }
+    }
+    get size() {
+        return this.current_size;
+    }
+    [Symbol.toStringTag];
+    [Symbol.iterator]() {
+        return this.entries();
+    }
+    *entries() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                let entry = this.map.get(key);
+                if (entry != null) {
+                    yield [key, entry.value];
+                }
+            }
+        }
+    }
+    *keys() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                yield key;
+            }
+        }
+    }
+    *values() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                let entry = this.map.get(key);
+                if (entry != null) {
+                    yield entry.value;
+                }
+            }
+        }
+    }
+    clear() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                this.registry.unregister(key);
+            }
+        }
+        this.keyrefs.clear();
+        this.map = new WeakMap();
+    }
+    delete(key) {
+        let entry = this.map.get(key);
+        if (entry == null) {
+            return false;
+        }
+        else {
+            this.map.delete(key);
+            this.keyrefs.delete(entry.keyref);
+            this.registry.unregister(key);
+            return true;
+        }
+    }
+    forEach(callback, self) {
+        for (let [key, value] of this) {
+            callback.call(self, value, key, this);
+        }
+    }
+    get(key) {
+        return this.map.get(key)?.value;
+    }
+    has(key) {
+        return this.map.has(key);
+    }
+    set(key, value) {
+        let entry = this.map.get(key);
+        if (entry == null) {
+            let keyref = new WeakRef(key);
+            this.registry.register(key, keyref, key);
+            this.current_size += 1;
+            entry = {
+                keyref,
+                value
+            };
+            this.keyrefs.add(keyref);
+            this.map.set(key, entry);
+        }
+        else {
+            entry.value = value;
+        }
+        return this;
+    }
+}
+exports.IterableWeakMap = IterableWeakMap;
+;
+class IterableWeakSet {
+    keyrefs;
+    registry;
+    map;
+    current_size;
+    constructor(iterable) {
+        this.keyrefs = new Set();
+        this.registry = new FinalizationRegistry((keyref) => {
+            this.keyrefs.delete(keyref);
+            this.current_size -= 1;
+        });
+        this.map = new WeakMap();
+        this.current_size = 0;
+        for (let key of iterable ?? []) {
+            this.add(key);
+        }
+    }
+    get size() {
+        return this.current_size;
+    }
+    [Symbol.toStringTag];
+    [Symbol.iterator]() {
+        return this.values();
+    }
+    *entries() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                yield [key, key];
+            }
+        }
+    }
+    *keys() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                yield key;
+            }
+        }
+    }
+    *values() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                yield key;
+            }
+        }
+    }
+    add(key) {
+        let entry = this.map.get(key);
+        if (entry == null) {
+            let keyref = new WeakRef(key);
+            this.registry.register(key, keyref, key);
+            this.current_size += 1;
+            entry = {
+                keyref
+            };
+            this.keyrefs.add(keyref);
+            this.map.set(key, entry);
+        }
+        return this;
+    }
+    clear() {
+        for (let keyref of this.keyrefs) {
+            let key = keyref.deref();
+            if (key != null) {
+                this.registry.unregister(key);
+            }
+        }
+        this.keyrefs.clear();
+        this.map = new WeakMap();
+    }
+    delete(key) {
+        let entry = this.map.get(key);
+        if (entry == null) {
+            return false;
+        }
+        else {
+            this.map.delete(key);
+            this.keyrefs.delete(entry.keyref);
+            this.registry.unregister(key);
+            return true;
+        }
+    }
+    forEach(callback, self) {
+        for (let key of this) {
+            callback.call(self, key, key, this);
+        }
+    }
+    has(key) {
+        return this.map.has(key);
+    }
+}
+exports.IterableWeakSet = IterableWeakSet;
 ;
