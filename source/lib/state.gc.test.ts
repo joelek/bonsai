1ae6a14706adacb35aee71627c0e59ee1b5080c8
Joel Ek <contact@joelek.se>
2025-06-06 11:39:06 +0200

    Add broken test for '.shadow().compute()'.

--- source/lib/state.gc.test.ts
+++ source/lib/state.gc.test.ts
@@ -24,3 +24,15 @@ wtf.test(`Subscribed states should be automatically reclaimed by the garbage col
 		globalThis.gc?.();
 	});
 });
+/*
+wtf.test(`Subscribed states should not be automatically reclaimed by the garbage collector when there are references around.`, async (assert) => {
+	let a = stateify(1 as number);
+	let b = a.shadow().compute((a) => a);
+	globalThis.gc?.();
+	await new Promise<void>((resolve, reject) => {
+		setTimeout(resolve, MAX_WAIT_MS);
+	});
+	a.update(2);
+	assert.equals(b.value(), 2);
+});
+ */
