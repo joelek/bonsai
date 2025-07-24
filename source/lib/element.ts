68bec7a5e88aeb611dc7754272e41c415a63c8fc
Joel Ek <contact@joelek.se>
2025-07-24 07:20:10 +0200

    Fix issue with async elements.

--- source/lib/element.ts
+++ source/lib/element.ts
@@ -61,11 +61,12 @@ export function createNode(value: Value | Node): Node {
 	if (value instanceof Node) {
 		return value;
 	} else if (value instanceof Promise) {
-		let placeholder = document.createTextNode("");
+		let container = document.createElement("div");
+		container.style.display = "contents";
 		value
 			.catch(() => undefined)
-			.then((value) => placeholder.replaceWith(createNode(value)));
-		return placeholder;
+			.then((value) => container.appendChild(createNode(value)));
+		return container;
 	} else {
 		return document.createTextNode(serializeValue(value));
 	}
