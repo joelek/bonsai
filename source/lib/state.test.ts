b80130c2df04e717afce2e0484d22fe9d61a4e2b
Joel Ek <contact@joelek.se>
2026-02-19 16:59:56 +0100

    Add tests.

--- source/lib/state.test.ts
+++ source/lib/state.test.ts
@@ -2706,3 +2706,23 @@ wtf.test(`Array states should update properly when observers are attached as a r
 	});
 	state.update(["two"]);
 });
+
+wtf.test(`Object states with members having type {} | undefined initialized as {} should emit update events when updated to undefined.`, (assert) => {
+	let state = make_state({ key: {} as {} | undefined });
+	let updated = false;
+	state.observe("update", (state) => {
+		updated = true;
+	});
+	state.update({ key: undefined });
+	assert.equals(updated, true);
+});
+
+wtf.test(`Object states with members having type {} | null initialized as {} should emit update events when updated to null.`, (assert) => {
+	let state = make_state({ key: {} as {} | null });
+	let updated = false;
+	state.observe("update", (state) => {
+		updated = true;
+	});
+	state.update({ key: null });
+	assert.equals(updated, true);
+});
